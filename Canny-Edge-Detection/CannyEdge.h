597e83ce4966eb54fb74ef40f45db440b253c4db
woodknight <lijiangpku@gmail.com>
2017-10-03 17:03:41 -0700

    fixed ifndef bugs

--- Canny-Edge-Detection/CannyEdge.h
+++ Canny-Edge-Detection/CannyEdge.h
@@ -1,6 +1,5 @@
 #ifndef _CANNYEDGE_H
 #define _CANNYEDGE_H
-#endif
 
 void GaussianFilter(const unsigned char *src, float *dst, const int w, const int h);
 void SobelEdge(const float *image, float *edgeAmp, int *edgeAngle, const int numOfRows, const int numOfCols);
@@ -9,3 +8,5 @@ void histogram(const float *image, int *hist, const int numOfRows, const int num
 unsigned char otsu(const int *hist);
 unsigned char percentile(const int *hist, const double p);
 void threshold(const float *image, bool *binaryImage, const int numOfRows, const int numOfCols);
+
+#endif
